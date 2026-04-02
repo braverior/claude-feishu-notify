9921fcc45b96f24862d3e6f076c4be08464e038a
lele <619923727@qq.com>
2026-04-02 18:09:06 +0800

    支持卸载命令

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,18 @@
 #!/usr/bin/env node
 import { startServer } from "./server.js";
 import { handleNotify } from "./notify.js";
-import { runSetup } from "./setup.js";
+import { runSetup, runUninstall } from "./setup.js";
 const command = process.argv[2];
 switch (command) {
     case "setup":
         runSetup().catch((err) => {
             console.error("Setup failed:", err.message);
+            process.exit(1);
+        });
+        break;
+    case "uninstall":
+        runUninstall().catch((err) => {
+            console.error("Uninstall failed:", err.message);
             process.exit(1);
         });
         break;
